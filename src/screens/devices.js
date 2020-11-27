17b8ddd3b897cdb93dc3cd1815321408f5685896
linleo345 <leolin2@illinois.edu>
2020-11-27 17:32:22 -0600

    Updates

--- src/screens/devices.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DevicesTab = () => {
-    return (
-        <div className={"jumbotron max-height"}>Devices Tab</div>
-    )
-}
-
-export default DevicesTab;
--- src/screens/dragdrop.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DragDropArea = () => {
-    return (
-        <div className={"jumbotron max-height"}>Drag-Drop Area</div>
-    )
-}
-
-export default DragDropArea;
